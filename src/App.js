132b8cbcc3853ab8eace5cc6dd3cbbea6abb9661
Andrew McMechan <coderwurst@gmail.com>
2019-07-18 17:18:13 +0200

    add search field

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+
 import './App.css';
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 512,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <h1>Lorem Ipsum</h1>
+      <p>Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Donec ac odio tempor orci dapibus ultrices.</p>
+      <TextField
+        id="search-box"
+        label="PLZ, Stadt, Straße und Hausnummer"
+        className={classes.textField}
+        margin="normal"
+      />
+      
+      <footer className="App-footer">coderwurst 2019</footer>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { cleanup, render } from '@testing-library/react'
+import { exportAllDeclaration } from '@babel/types';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  afterEach(cleanup)
+  
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders search field', () => {
+    const { queryByLabelText } = render(<App/>)
+    expect(queryByLabelText('PLZ, Stadt, Straße und Hausnummer')).toBeTruthy()
+  });
